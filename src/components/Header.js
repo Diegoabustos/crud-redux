3115b572847447b82c8a02fa3134bc4707fcf1a8
Diegoabustos <diegoabustos24@gmail.com>
2021-09-30 20:22:05 -0500

    Creando el Formulario para Nuevos Productos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    return ( 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
-            <div className="container" >
-                <h1>
-                    <Link to={'/'} className="text-light" >
-                        CRUD - React, Redux, REST API & Axios
-                    </Link>
-                </h1>
-            </div>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
+      <div className="container">
+        <h1>
+          <Link to={"/"} className="text-light">
+            CRUD - React, Redux, REST API & Axios
+          </Link>
+        </h1>
+      </div>
 
-            <Link to={"/productos/nuevo"} className="btn btn-danger nuevo-post d-block d-md-inline-block" >Agregar Producto &#43;</Link>
+      <Link
+        to={"/productos/nuevo"}
+        className="btn btn-danger nuevo-post d-block d-md-inline-block"
+      >
+        Agregar Producto &#43;
+      </Link>
+    </nav>
+  );
+};
 
-        </nav>
-     );
-}
- 
 export default Header;
